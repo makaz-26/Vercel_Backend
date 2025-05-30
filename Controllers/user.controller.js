e71c1242626ce2d3baf060bed3cc3e4bc938d772
makaz-26 <makaz4504@gmail.com>
2025-05-30 16:37:19 +0530

    Updated userModel

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../Models/user.model");
 const { validationResult } = require("express-validator");
 const { uploadOnCloudinary } = require("../utils/cloudinary");
 const gpuPlans = require("../utils/gpuPlans");
